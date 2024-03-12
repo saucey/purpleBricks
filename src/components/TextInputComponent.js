8e5159fa1788109c62f0d6b35e4c52b394743358
lsnomensa <lscott@nomensa.com>
2024-03-12 10:27:03 +0000

    component modified done

--- src/components/TextInputComponent.js
+++ src/components/TextInputComponent.js
@@ -1,8 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react';
-import '../App.css';
 import { FormDataContext } from '../globalState/FormDataContext';
 import reportFormData from '../structure.json';
-import { ButtonCta, SecondaryButton, TextInput } from 'wmca-shared-components';
+import { ButtonCta, TextInput } from 'wmca-shared-components';
 
 const TextInputComponent = () => {
   const [formDataState, formDataDispatch] = useContext(FormDataContext);
@@ -15,141 +14,82 @@ const TextInputComponent = () => {
     const currentPageData = reportFormData.pages.find(page => page.id === currentStep);
     setPageData(currentPageData);
 
-    if (pageData && formData.length > 0) {
+    setInputValues({});
+    if (currentPageData && formData.length > 0) {
       const matchingData = formData.find(data => data.pageId === currentStep);
-      if (matchingData) {
-        setInputValues(matchingData.inputValues);
-      } else {
-        const initialInputValues = pageData.options.reduce((acc, option) => {
-          acc[option.name] = '';
-          return acc;
-        }, {});
-        setInputValues(initialInputValues);
-      }
-    } else {
-      setInputValues({});
+      setInputValues(matchingData ? matchingData.inputValues : currentPageData.options.reduce((acc, option) => {
+        acc[option.name] = '';
+        return acc;
+      }, {}));
     }
-  }, [currentStep, formData, pageData]);
+  }, [currentStep, formData]);
 
   const redirect = () => {
-
     const initialErrors = {};
 
-    pageData.options.forEach(option => {
-      const { name } = option;
+    pageData.options.forEach(({ name, type }) => {
       const value = inputValues[name];
-      const validationRules = pageData?.validation?.rules;
-      if (validationRules && validationRules[name]?.required) {
-        console.log(value, 'value')
-        initialErrors[name] = !value || !value.trim();
-      }
-
-      if (option.type === 'email' && value) {
-        initialErrors[name] = !isValidEmail(value);
-      }
-
+      const required = pageData.validation?.rules?.[name]?.required;
+      if (required) initialErrors[name] = !value || !value.trim();
+      if (type === 'email' && value) initialErrors[name] = !isValidEmail(value);
     });
 
     setErrors(initialErrors);
 
-    // Loop through the errors object
-    for (const errorKey in initialErrors) {
-      if (initialErrors.hasOwnProperty(errorKey) && initialErrors[errorKey]) {
-        // If any property is true, it means there's an error, stop the redirection
-        console.log("Code execution stopped due to validation errors.");
-        return;
-      }
-    }
+    if (Object.values(initialErrors).some(error => error)) return;
 
-    // Continue with form submission if no errors
-    console.log("Validation passed. Proceeding with form submission.");
-
-    const payload = {
-      inputValues,
-      pageId: pageData.id
-    };
-
+    const payload = { inputValues, pageId: pageData.id };
     let nextPage = currentStep === 2 ? formDataState.futureStep : pageData.nextId;
+    if (formDataState.answerChecks) nextPage = 22;
 
-    if (formDataState.answerChecks) {
-      nextPage = 22;
-    }
-
-    formDataDispatch({
-      type: 'UPDATE_STEP',
-      payload: { nextStep: nextPage, currentStep },
-    });
-
-    formDataDispatch({
-      type: 'UPDATE_FORM_DATA',
-      payload,
-    });
+    formDataDispatch({ type: 'UPDATE_STEP', payload: { nextStep: nextPage, currentStep } });
+    formDataDispatch({ type: 'UPDATE_FORM_DATA', payload });
   };
 
-
-  const handleInputChange = (event, name, type) => {
-    const { value } = event.target;
+  const handleInputChange = ({ target: { value } }, name, type) => {
     setInputValues({ ...inputValues, [name]: value });
-
-    const validationRules = pageData?.validation?.rules;
-    if (validationRules && validationRules[name]?.required) {
-      setErrors({ ...errors, [name]: !value || !value.trim() });
-    }
-
-    if (type === 'email' && value) {
-      const isValid = isValidEmail(value);
-      setErrors({ ...errors, [name]: !isValid });
-    }
-    
+    if (type === 'email' && value) setErrors(prevErrors => ({ ...prevErrors, [name]: !isValidEmail(value) }));
+    else setErrors(prevErrors => ({ ...prevErrors, [name]: pageData.validation?.rules?.[name]?.required && (!value || !value.trim()) }));
   };
 
-  const isValidEmail = (email) => {
-    // Simple email validation regex
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const valid = emailRegex.test(email);
-    return valid
-  };
+  const isValidEmail = email => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
 
   return (
-    <>
-      {pageData && (
-        <div className="wmnds-col-1 wmnds-p-lg wmnds-bg-white">
-          <h1>current {currentStep}</h1>
-          <div className="wmnds-progress-indicator">
-            Section {pageData.section} of 2
-            <h4>About the issue</h4>
-          </div>
-          <h1 className="heading-2">{currentStep === 2 && futureStep === 3 ? pageData.title2 : pageData.title}</h1>
-          {pageData.info && (
-            <>
-              <div className="wmnds-warning-text wmnds-m-b-lg">
-                <svg className="wmnds-warning-text__icon">
-                  <use xlinkHref="#wmnds-general-warning-circle" href="#wmnds-general-warning-circle"></use>
-                </svg>
-                <span dangerouslySetInnerHTML={{ __html: pageData.info }} />
-              </div>
-            </>
-          )}
-
-          {pageData.options.map((option, index) => (
-            <TextInput
-              key={index}
-              id={option.name}
-              name={option.name}
-              type={option.type || 'text'}
-              label={<span dangerouslySetInnerHTML={{ __html: option.label }} />}
-              errorMessage={errors[option.name] && (option.type === 'email' && inputValues[option.name] !== '' ? "Please enter a valid email address" : "This field is required")}
-              isError={errors[option.name]}
-              value={inputValues[option.name]}
-              onChange={event => handleInputChange(event, option.name, option.type)}
-              placeholder="Enter your value"
-            />
-          ))}
-
-          <ButtonCta label="Continue" onClick={() => redirect()} />
+    pageData && (
+      <div className="wmnds-col-1 wmnds-p-lg wmnds-bg-white">
+        <h1>current {currentStep}</h1>
+        <div className="wmnds-progress-indicator">
+          Section {pageData.section} of 2
+          <h4>About the issue</h4>
         </div>
-      )}
-    </>
+        <h1 className="heading-2">{currentStep === 2 && futureStep === 3 ? pageData.title2 : pageData.title}</h1>
+        {pageData.info && (
+          <div className="wmnds-warning-text wmnds-m-b-lg">
+            <svg className="wmnds-warning-text__icon">
+              <use xlinkHref="#wmnds-general-warning-circle" href="#wmnds-general-warning-circle"></use>
+            </svg>
+            <span dangerouslySetInnerHTML={{ __html: pageData.info }} />
+          </div>
+        )}
+
+        {pageData.options.map(({ name, type, label }, index) => (
+          <TextInput
+            key={index}
+            id={name}
+            name={name}
+            type={type || 'text'}
+            label={<span dangerouslySetInnerHTML={{ __html: label }} />}
+            errorMessage={errors[name] && (type === 'email' && inputValues[name] !== '' ? "Please enter a valid email address" : "This field is required")}
+            isError={errors[name]}
+            value={inputValues[name]}
+            onChange={e => handleInputChange(e, name, type)}
+            placeholder="Enter your value"
+          />
+        ))}
+
+        <ButtonCta label="Continue" onClick={redirect} />
+      </div>
+    )
   );
 };
 
